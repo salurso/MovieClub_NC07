ba896b1021968634eb464555e6b0f95dcb5ff071
Vittorio <v.guida16@studenti.unisa.it>
2024-01-17 17:47:04 +0100

    Modifica dataBase

---
+++ src/main/java/application/entity/Persona.java
@@ -0,0 +1,81 @@
+package application.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Persona {
+
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private boolean admin;
+
+    private Set<Film> watchlist;
+
+    public Persona(String nome, String cognome, String email, String password) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.password = password;
+        this.admin = false;
+        watchlist = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public Set<Film> getWatchlist() {
+        return watchlist;
+    }
+
+    public void setWatchlist(Set<Film> watchlist) {
+        this.watchlist = watchlist;
+    }
+}
