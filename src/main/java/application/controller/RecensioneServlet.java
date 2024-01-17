6581d0f061a3d195b995505fa3321fcdbb520838
g-faraco1 <g.faraco1@studenti.unisa.it>
2024-01-17 11:16:33 +0100

    Update RecensioneServlet.java

--- src/main/java/application/controller/RecensioneServlet.java
+++ src/main/java/application/controller/RecensioneServlet.java
@@ -1,7 +1,24 @@
 package application.controller;
 
-public class RecensioneServlet {
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "RecensioneServlet", value = "/RecensioneServlet")
+public class RecensioneServlet extends HttpServlet{
 
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher ds = request.getRequestDispatcher("/WEB-INF/gui/recensione.jsp");
+        ds.forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
 
 }
